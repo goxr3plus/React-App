e0ebcfc9e47316d9da80614b2296366c1db75954
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-01-31 16:31:24 +0200

    Added Person.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person.js'
 
 class App extends Component {
+  
   render (){
-      return React.createElement('div',null,React.createElement('h1',null,"Hello I'am John"));
+     return (
+      <div className="App">
+        <h1> Super Reactor boy</h1>
+        <Person />
+      </div>
+     );
+
+    //  return React.createElement('div',{className:'App'},React.createElement('h1',null,"Hello I'am John"));
   }
 
 }
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const person = (props) =>{
+    return <p> I'm a YUNG WEB WORKER {props[1]}</p>
+}
+
+export default person;
