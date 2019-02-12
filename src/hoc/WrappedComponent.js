ebd41ba3194196438762de198a2d9d8b560fcf8b
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2019-02-12 12:07:29 +0200

    Finishing with HOCS

--- src/hoc/WrappedComponent.js
+++ src/hoc/WrappedComponent.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React , {Component} from 'react';
 
 const wrappedComponent = (WrappedComponent, className) => {
-    return (props) => (
-        <div className={className}>
-            <WrappedComponent/>
-        </div>
-    )
+    return class extends Component {
+        render(){
+           return (
+             <div className={className}>
+                <WrappedComponent {...this.props}/>
+             </div>
+           )
+        }   
+    } 
 }
 
 export default wrappedComponent;
